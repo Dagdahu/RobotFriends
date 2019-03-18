edeff584b41e4e0f2d5735ad804e527b8f64a358
HugoC <chatelhugo@hotmail.fr>
2019-03-18 08:44:24 +0100

    Project built and ready for production

---
+++ build/precache-manifest.70753c023c8b6a98104f054110c78585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c16ca397ca36028f946f828e788ad204",
+    "url": "/static/media/robotaur.c16ca397.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "d0c3146d72b68dadfd85",
+    "url": "/static/js/main.3537fa4f.chunk.js"
+  },
+  {
+    "revision": "a0a32739855839b72a53",
+    "url": "/static/js/2.1e707426.chunk.js"
+  },
+  {
+    "revision": "d0c3146d72b68dadfd85",
+    "url": "/static/css/main.ab80e53b.chunk.css"
+  },
+  {
+    "revision": "a0a32739855839b72a53",
+    "url": "/static/css/2.d723db55.chunk.css"
+  },
+  {
+    "revision": "a31586eabd74b3d85c4ac039aa18f498",
+    "url": "/index.html"
+  }
+];
