54486d0f99fa93080c03f98e52347def26964931
HugoC <chatelhugo@hotmail.fr>
2019-03-19 11:33:49 +0100

    Updates

--- precache-manifest.da8aa3763b3621ee7002c25a566aab05.js
+++ precache-manifest.da8aa3763b3621ee7002c25a566aab05.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/RobotFriends/static/js/runtime~main.601a5031.js"
   },
   {
-    "revision": "b5f8a1072945e75a2bba",
-    "url": "/RobotFriends/static/js/main.b0bd415b.chunk.js"
+    "revision": "2097ab53e5d3171bd867",
+    "url": "/RobotFriends/static/js/main.73650ce5.chunk.js"
   },
   {
     "revision": "4781ae139294fc13d0a4",
     "url": "/RobotFriends/static/js/2.53bf819f.chunk.js"
   },
   {
-    "revision": "b5f8a1072945e75a2bba",
+    "revision": "2097ab53e5d3171bd867",
     "url": "/RobotFriends/static/css/main.a50b4ce6.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/RobotFriends/static/css/2.d723db55.chunk.css"
   },
   {
-    "revision": "57aef4d9d2e5e7d3ebe26cc530bd3d3d",
+    "revision": "1f8a4de9efb24fccdad757281e7cd44b",
     "url": "/RobotFriends/index.html"
   }
 ];
