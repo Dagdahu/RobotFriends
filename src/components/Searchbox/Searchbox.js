53d36d3e293ed72172af40d307256588804e9317
HugoC <chatelhugo@hotmail.fr>
2019-03-19 15:55:21 +0100

    PWA end of exercise

--- src/components/Searchbox/Searchbox.js
+++ src/components/Searchbox/Searchbox.js
@@ -3,17 +3,15 @@ import React from 'react';
 class Searchbox extends React.Component {
     render () {
         return (
-            <div>
-                <label>
-                    Search Robots : 
-                    <input
-                        className='pa2 ma3' 
-                        type='text'
-                        placeholder='search'
-                        onChange={e => this.props.onSearchChange(e.target.value)}
-                    />
-                </label>
-            </div>
+            <label>
+                 
+                <input
+                    className='pa2 ma3' 
+                    type='text'
+                    placeholder='search'
+                    onChange={e => this.props.onSearchChange(e.target.value)}
+                />
+            </label>
         )
     }
 }
